259d9efebb74ca144cd2c551310b471a480ffd56
Michael Kastner <kastner@galt.de>
2019-11-24 14:26:50 +0100

    Initial commit

---
+++ src/mk-vue-flex-text.vue
@@ -0,0 +1,4 @@
+<template src="./mk-vue-flex-text.html" lang="html"></template>
+<script src="./mk-vue-flex-text.js"></script>
+<style src="./mk-vue-flex-text.scss" lang="scss"></style>
+<style src="./mk-vue-flex-text-scoped.scss" lang="scss" scoped></style>
