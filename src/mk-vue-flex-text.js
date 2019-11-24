d6c73d2916ae1d96bd0e53679cd522eb787be55d
Michael Kastner <kastner@galt.de>
2019-11-24 17:16:34 +0100

    Basic Stuff

--- src/mk-vue-flex-text.js
+++ src/mk-vue-flex-text.js
@@ -1,6 +1,28 @@
 export default {
+  props: ['value'], 
   data() {
     return {
+      innerValue: ''
     };
+  },
+  mounted() {
+    this.innerValue = `${this.value}`; //clone value 
+  },
+  methods: {
+    removeTrailingWhitespace(s) {
+      const sanitized = s ? s : ''; 
+      return sanitized.replace(/^\s+|\s+$/g, '');
+    },
+    updateValue() {
+      const cleanedText = this.removeTrailingWhitespace(this.$el.innerText); 
+      if (this.model !== cleanedText) { 
+        this.$emit('input', { 
+          target: { 
+            type: 'input',
+            originalTarget: this.$el,
+            value: cleanedText} 
+        } );
+      }
+    } 
   }
 };
